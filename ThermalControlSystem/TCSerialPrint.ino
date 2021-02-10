4f98ddf2fba82b3db6d9c98a5b01a89256039026
graemenewton <73535025+graemenewton@users.noreply.github.com>
2021-02-10 14:28:44 +0000

    Create TCSerialPrint.ino
    restore TC Serial print

---
+++ ThermalControlSystem/TCSerialPrint.ino
@@ -0,0 +1,16 @@
+/* CUSTOM FUNCTION FOR PRINT THERMOCUPLE PARAMETERS TO SERIAL INTERFACE */
+
+/* Print temperature values to Serial Bus and LCD. Hot Junction is the probe temp, Cold Junction is the reference built into the Adafruit MCP9600 I2C board  */
+// Serial.print(value, 4) prints 4 decimal places if value is float for example
+
+
+void TCSerialPrint()
+{
+
+  Serial.print("Hot Junction 1: "); Serial.println(mcp1.readThermocouple(), 4); Serial.print(" ºC");
+  Serial.print("Cold Junction 1: "); Serial.println(mcp1.readAmbient(), 4); Serial.print(" ºC");
+  Serial.print("Hot Junction 2: "); Serial.println(mcp2.readThermocouple(), 4); Serial.print(" ºC");
+  Serial.print("Cold Junction 2: "); Serial.println(mcp2.readAmbient(), 4); Serial.print(" ºC");
+  Serial.print("ADC: "); Serial.print(mcp1.readADC() * 2, 4); Serial.println(" uV");
+  Serial.print("ADC: "); Serial.print(mcp2.readADC() * 2, 4); Serial.println(" uV");
+}
