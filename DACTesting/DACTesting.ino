1d34517249282fdf5545df990205431f3d775cbc
graemenewton <73535025+graemenewton@users.noreply.github.com>
2021-04-06 14:06:26 +0100

    Update DACTesting.ino
    Got MCP4725 adafruit dac tested and working

--- DACTesting/DACTesting.ino
+++ DACTesting/DACTesting.ino
@@ -1,30 +1,41 @@
-     
-const int lowPin = 2;
-const int DACInPin = 23; // DIG PIN 23 for bnc in       
-const int APin = A0;
+/*script successfully writes a voltage to the DAC which reads accurately on the A0
+pin with respecto to the commanded voltage*/
+ 
+
+#include <Wire.h>
+#include <Adafruit_MCP4725.h>
+
+Adafruit_MCP4725 dac;
+
+#define DAC_RESOLUTION (9)
+
+const int DACInPin = A0; // DIG PIN 23 for bnc in       
 float AFloat;
 float voltage = 0;
+
+
 void setup() {
 
   pinMode(DACInPin, INPUT);
-  pinMode(lowPin, OUTPUT);
   Serial.begin(9600);
+  Serial.print("Hello World!");
 
   analogReadResolution(12);
   analogWriteResolution(12);
+
+  dac.begin(0x63);
+
+  delay(500);
 }
 
 void loop() {
-  digitalWrite(lowPin, LOW);
-  AFloat = analogRead(0);
+
+
+dac.setVoltage(4095/2, false);
+  AFloat = analogRead(DACInPin);
   voltage = (AFloat*3.3/4095);
-Serial.print(digitalRead(DACInPin)); Serial.print(" "); Serial.print(analogRead(APin)); Serial.print(" "); Serial.println(voltage);
-delay(100);
+Serial.print(digitalRead(DACInPin)); Serial.print(" "); Serial.print(analogRead(DACInPin)); Serial.print(" "); Serial.println(voltage);
+delay(1);
 
 
 }
-
-//digital read displays as "1" aka HIGH, when pin voltage in is above 1.4V (approx 438 on integer scale)
-//tested level shifter, works nicely. The Voltage put out is the same as that supplied to the VccA and VccB pins, so each side will put out whatever the main pins are.
-// essentially this means any TTL voltage can be supplied to the 5v side, and 3.3v will be supplied to the arduino.
-//however, ttl out must be either 3.3v or 5v. 2 DAC outs? one at each? to let user choose 3.3v or 5v ttl to hook up to?
