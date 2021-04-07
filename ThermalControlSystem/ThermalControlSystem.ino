cac6a751a7b6968a266332db94ae419ffca2bd03
graemenewton <73535025+graemenewton@users.noreply.github.com>
2021-04-07 11:18:48 +0100

    Update ThermalControlSystem.ino
    Formatting

--- ThermalControlSystem/ThermalControlSystem.ino
+++ ThermalControlSystem/ThermalControlSystem.ino
@@ -1,24 +1,24 @@
- //Useful notes: Arduino due uses 3.3V logic, digital pins output 130mA.
- // 10 bits resolution returns values between 0-1023, 8 bits resolution returns values between 0-255, 12 bits resolution returns values between 0-4096
+//Useful notes: Arduino due uses 3.3V logic, digital pins output 130mA.
+// 10 bits resolution returns values between 0-1023, 8 bits resolution returns values between 0-255, 12 bits resolution returns values between 0-4096
 
 
 //direct from MCP_9600 github, converstion to celcius is by *0.0625
- /* float Adafruit_MCP9600::readThermocouple(void) {      
- // if (!enabled()) {
- // return NAN; }
+/* float Adafruit_MCP9600::readThermocouple(void) {
+  // if (!enabled()) {
+  // return NAN; }
 
- // define the register
- // Adafruit_I2CRegister therm_reg =
- //   Adafruit_I2CRegister(i2c_dev, MCP9600_HOTJUNCTION, 2, MSBFIRST);
+  // define the register
+  // Adafruit_I2CRegister therm_reg =
+  //   Adafruit_I2CRegister(i2c_dev, MCP9600_HOTJUNCTION, 2, MSBFIRST);
 
- // read a signed 16 bit value
- // int16_t therm = therm_reg.read();
- 
- // convert to floating and shift to celsius
- // float temp = therm;
- // temp *= 0.0625; // 0.0625*C per LSB!
- // return temp;
- */
+  // read a signed 16 bit value
+  // int16_t therm = therm_reg.read();
+
+  // convert to floating and shift to celsius
+  // float temp = therm;
+  // temp *= 0.0625; // 0.0625*C per LSB!
+  // return temp;
+*/
 
 
 
@@ -32,13 +32,13 @@
 
 /* Analog Pins */
 const int PotPin1 = A0; //10 turn potentiometer
-const int PotPin2 = A1; //3 turn potentiometer 
+const int PotPin2 = A1; //3 turn potentiometer
 
 
 /* Digital Pins */
 //const int sets constant integers, such as pin numbers.
 
-//PWM Pins 
+//PWM Pins
 const int HeatPWMPin = 2;
 const int FanPWMPin = 3;
 const int PeltierLPWMPin = 4;
@@ -66,7 +66,7 @@ const int PeltierLEnablePin = 36;
 const int PeltierREnablePin = 37;
 //LEDs
 const int RedLEDPin = 45;
-const int GreenLEDPin = 46; 
+const int GreenLEDPin = 46;
 const int YellowLEDPin = 47;
 //Button LEDs
 const int GreenButtonLEDPin = 48;
@@ -86,8 +86,8 @@ int TempLEDBluePWM;
 int LEDBrightness;
 
 //Floats
-float T1Temp; //T1Temp is heat exchanger, store as float T1Temp //note that floats must be used in equations as dividing integers will cause errors as they 
-              //cannot have decimal points.
+float T1Temp; //T1Temp is heat exchanger, store as float T1Temp //note that floats must be used in equations as dividing integers will cause errors as they
+//cannot have decimal points.
 float T2Temp; //T2Temp is bath, store as float T2Temp // e.g. 7/2 is 3.5 but this will not work with integer notation
 
 float BaselineTemp = 32.0;
@@ -102,10 +102,10 @@ unsigned long PreviousLEDTime = 0;
 unsigned long LEDInterval = 100;
 
 /* Syntax for LCD pin numbers:
-LiquidCrystal(rs, enable, d4, d5, d6, d7)
-LiquidCrystal(rs, rw, enable, d4, d5, d6, d7) <------------- this one used
-LiquidCrystal(rs, enable, d0, d1, d2, d3, d4, d5, d6, d7)
-LiquidCrystal(rs, rw, enable, d0, d1, d2, d3, d4, d5, d6, d7) */
+  LiquidCrystal(rs, enable, d4, d5, d6, d7)
+  LiquidCrystal(rs, rw, enable, d4, d5, d6, d7) <------------- this one used
+  LiquidCrystal(rs, enable, d0, d1, d2, d3, d4, d5, d6, d7)
+  LiquidCrystal(rs, rw, enable, d0, d1, d2, d3, d4, d5, d6, d7) */
 
 LiquidCrystal lcd(LCDRS, LCDRW, LCDEnable, LCDD4, LCDD5, LCDD6, LCDD7); //set the LCD pins (as described in above pin map)
 
@@ -121,9 +121,9 @@ void setup()
 {
 
 
-/* Set the pin modes. */
+  /* Set the pin modes. */
   pinMode(PotPin1, INPUT); //10 turn potentiometer
-  pinMode(PotPin2, INPUT); //3 turn potentiometer 
+  pinMode(PotPin2, INPUT); //3 turn potentiometer
 
 
   //PWM Pins
@@ -164,28 +164,28 @@ void setup()
   pinMode(YellowButtonLED3Pin, OUTPUT);
   pinMode(YellowButtonLED4Pin, OUTPUT);
 
-/*Set LEDs etc on for setup profile*/
+  /*Set LEDs etc on for setup profile*/
   digitalWrite(RedLEDPin, HIGH);
   digitalWrite(GreenLEDPin, LOW);
   digitalWrite(YellowLEDPin, LOW);
-  
 
-/* Print startup message to LCD. */
+
+  /* Print startup message to LCD. */
   lcd.begin(20, 4); //begin LCD communication, device has 20 coloumbs and 4 rows
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("  Thermal Control");
-  lcd.setCursor(0,2);
+  lcd.setCursor(0, 2);
   lcd.print("System Initialising");
   delay(2000);
 
-/* Begin serial communication at 115200 baud */
+  /* Begin serial communication at 115200 baud */
 
   Serial.begin(115200); //start serial communication with computer at 115200 baud rate
   while (!Serial) { //wait until serial has started (this may need to be removed if the arduino will be used without being attached to USB)
     delay(10); //slight delay to give breathing time to arduino when checking if serial connected
   }
 
-/* Print setup information to the LCD and serial communication port. */
+  /* Print setup information to the LCD and serial communication port. */
 
   Serial.println("Checking Thermocouple"); //print that we are testing the thermcouples to check if present
   lcd.clear(); //clear lcd if anything on
@@ -196,8 +196,8 @@ void setup()
   delay(2000); //wait for 2 secs before starting new command to allow message to be on screen for long enough for user
   lcd.clear(); //clear lcd
 
-/* Initialise the driver with I2C_ADDRESS and the default I2C bus for TC1. */
-/* Thermcouple 1 checks. */
+  /* Initialise the driver with I2C_ADDRESS and the default I2C bus for TC1. */
+  /* Thermcouple 1 checks. */
 
   if (! mcp1.begin(TC1_Address)) { //if TC1 MCP9600 not detected then message will be printed and program will pause
     Serial.println("Thermocouple 1 not found!"); //will print message in the serial monitor and pause program
@@ -206,9 +206,9 @@ void setup()
     lcd.print("   Thermocouple 1   ");
     lcd.setCursor(0, 1);
     lcd.print("     Not Found!     ");
-    lcd.setCursor(0,2);
+    lcd.setCursor(0, 2);
     lcd.print("Check connections");
-    lcd.setCursor(0,3);
+    lcd.setCursor(0, 3);
     lcd.print("then reset Arduino.");
     while (1); //infinite loop. Pauses program here.
   }
@@ -223,8 +223,8 @@ void setup()
   lcd.clear();
   delay(500);
 
-/* Initialise the driver with I2C_ADDRESS and the default I2C bus for TC2. */
-/* Thermcouple 2 checks. */
+  /* Initialise the driver with I2C_ADDRESS and the default I2C bus for TC2. */
+  /* Thermcouple 2 checks. */
 
   if (! mcp2.begin(TC2_Address)) {
     Serial.println("Thermocouple 2 not found!"); //will print message in the serial monitor and pause program if MCP9600 cannot be detected
@@ -232,9 +232,9 @@ void setup()
     lcd.print("   Thermocouple 2   ");
     lcd.setCursor(0, 1);
     lcd.print("     Not Found!     ");
-    lcd.setCursor(0,2);
+    lcd.setCursor(0, 2);
     lcd.print("Check connections");
-    lcd.setCursor(0,3);
+    lcd.setCursor(0, 3);
     lcd.print("then reset Arduino.");
     while (1); //infinite loop. Pauses program here.
   }
@@ -249,7 +249,7 @@ void setup()
   lcd.clear();
   delay(500);
 
-/* Set ADC resolution for the MCP9600 modules and print to serial bus. */
+  /* Set ADC resolution for the MCP9600 modules and print to serial bus. */
 
   mcp1.setADCresolution(MCP9600_ADCRESOLUTION_18); // set resolution of mcp1 to 18 bits
   mcp2.setADCresolution(MCP9600_ADCRESOLUTION_18); // set resolution of mcp2 to 18 bits
@@ -272,7 +272,7 @@ void setup()
   }
   Serial.println(" bit"); //print the units, in this case bits, after the resultion
 
-/* Print thermcouple types to serial bus. */
+  /* Print thermcouple types to serial bus. */
 
   mcp1.setThermocoupleType(MCP9600_TYPE_T); //set TC1 to T-type theromcouple
   Serial.print("TC1 Thermocouple type set to "); //print to serial monitor
@@ -302,7 +302,7 @@ void setup()
   }
   Serial.println(" type"); //print to serial monitor
 
-/* Set filter coefficients for the thermocouples to 0 - meaning no moving average filter applied. */
+  /* Set filter coefficients for the thermocouples to 0 - meaning no moving average filter applied. */
 
   mcp1.setFilterCoefficient(0); //moving average filter coef set to 0, no filtering desired
   Serial.print("TC1 Filter coefficient value set to: ");
@@ -313,7 +313,7 @@ void setup()
   Serial.println(mcp2.getFilterCoefficient());
 
 
-/* Print the thermocouple type, filter coefficient and ADC resolution to the LCD for the user to see. */
+  /* Print the thermocouple type, filter coefficient and ADC resolution to the LCD for the user to see. */
 
   lcd.setCursor(0, 0);
   lcd.print("TC1 = ");
@@ -367,19 +367,19 @@ void setup()
   delay(5000); //wait to allow user to read
   lcd.clear();
 
-/* Code can be added here to set alerts. These can be used for safety features such as to shut off in the case of overheating. */
+  /* Code can be added here to set alerts. These can be used for safety features such as to shut off in the case of overheating. */
 
   mcp1.setAlertTemperature(1, 60); //an exmaple of how an alert can be set, e.g. for auto shut off if temps hit over 50
   Serial.print("Alert #1 temperature set to ");
   Serial.println(mcp1.getAlertTemperature(1)); //get alert 1 temperature from TC1 MCP9600
   mcp1.configureAlert(1, true, true);  // alert 1 enabled, rising temp
 
-/* Enable the two MCP9600 I2C devices. */
+  /* Enable the two MCP9600 I2C devices. */
 
   mcp1.enable(true);
   mcp2.enable(true);
 
-/* Print ready to operate message and turn red light off + green light on. */
+  /* Print ready to operate message and turn red light off + green light on. */
 
   Serial.println(F("------------------------------")); //divide temp recording data from setup info
 
@@ -391,49 +391,50 @@ void setup()
   digitalWrite(GreenLEDPin, HIGH);
   delay(1000); //slight delay before starting loop, the message will be displayed until something new is overwritten
 
-/* The ADC and DAC resolution is set to 10 bit by default for compatability purposes with other arduino boards, however, the Due is capable of higher resolution. */
+  /* The ADC and DAC resolution is set to 10 bit by default for compatability purposes with other arduino boards, however, the Due is capable of higher resolution. */
   analogReadResolution(12); //12 bits is maximum resolution which can be run on the Arduino Due, this command will unlock the best possible ADC resolution for us.
   analogWriteResolution(12); //12 bits is maximum resolution which can be run on the Arduino Due, this command will unlock the best possible DAC/PWM resolution for us.
   // analogWrite() is 8 bits by default (0-255), changing this means it will now be 0-4095. analogRead() is 10 bits (0-1023) by default, chaning this means it
   // would now be 0-4095. The map() function is useful here, e.g. map(sensorVal, 0, 1023, 0, 4095) - this would essentially scale 10 bits to 12 bits.
 
 
-dac.begin(0x63); //begin communications with the MCP4725 dac module at I2C address 0x62
- 
-  
+  dac.begin(0x63); //begin communications with the MCP4725 dac module at I2C address 0x62
+
+
 }
 
 
 void loop()
 {
-CurrentTime = millis(); //store the millis() value as the current time
+  
+  CurrentTime = millis(); //store the millis() value as the current time
 
-if (CurrentTime - PreviousDisplayTime > DisplayInterval) // display interval is 100ms, so: if 100ms has passed, then the lcd and serial monitors will print info
-{
-  PreviousDisplayTime = CurrentTime; //update the previous display time with the current time
-TCSerialPrint(); //uses custom function to print Theromcouple paramaters to Serial port. See TCSerialPrint.ino tab
-TCLCDPrint(); //uses custom function to print Theromcouple parameters to LCD display. See TCLCDPrint.ino tab
-}
+  if (CurrentTime - PreviousDisplayTime > DisplayInterval) // display interval is 100ms, so: if 100ms has passed, then the lcd and serial monitors will print info
+  {
+    PreviousDisplayTime = CurrentTime; //update the previous display time with the current time
+    TCSerialPrint(); //uses custom function to print Theromcouple paramaters to Serial port. See TCSerialPrint.ino tab
+    TCLCDPrint(); //uses custom function to print Theromcouple parameters to LCD display. See TCLCDPrint.ino tab
+  }
 
-/* Store temperatures for T1 and T2 as variables */
+  /* Store temperatures for T1 and T2 as variables */
   T1Temp = mcp1.readThermocouple(); //T1Temp is heat exchanger, store as float T1Temp
   T2Temp = mcp2.readThermocouple(); //T2Temp is bath, store as float T2Temp
 
-if (CurrentTime - PreviousLEDTime > LEDInterval) // LED interval is 100ms. so if 100ms pass, then update the LED Colour
-{
-  PreviousLEDTime = CurrentTime; //update the previous LED time with the current time
-  
-/* Code below regultes the RGB PWM numbers for the RGB LED - this is used so operators can guage the temperature based on LED colour from a distance 
-without having to get close to the screen. */
-LEDTempColour(); //custom function for assigning PWM integers based on bath temperature for LED colouring, function defined in LEDTempColour.ino Tab
-LEDBrightness = (4095-analogRead(A1)); //99 
-  
-//use PWM numbers set above to control colour of RGB LED.
-  analogWrite(TempLEDRedPin, (map(TempLEDRedPWM, 0, 255, 0, 4095)/map(LEDBrightness, 0, 4095, 1, 10)));
-  analogWrite(TempLEDGreenPin, (map(TempLEDGreenPWM, 0, 255, 0, 4095)/map(LEDBrightness, 0, 4095, 1, 10)));
-  analogWrite(TempLEDBluePin, (map(TempLEDBluePWM, 0, 255, 0, 4095)/map(LEDBrightness, 0, 4095, 1, 10)));
-}
+  if (CurrentTime - PreviousLEDTime > LEDInterval) // LED interval is 100ms. so if 100ms pass, then update the LED Colour
+  {
+    PreviousLEDTime = CurrentTime; //update the previous LED time with the current time
+
+    /* Code below regultes the RGB PWM numbers for the RGB LED - this is used so operators can guage the temperature based on LED colour from a distance
+      without having to get close to the screen. */
+    LEDTempColour(); //custom function for assigning PWM integers based on bath temperature for LED colouring, function defined in LEDTempColour.ino Tab
+    LEDBrightness = (4095 - analogRead(A1)); //99
+
+    //use PWM numbers set above to control colour of RGB LED.
+    analogWrite(TempLEDRedPin, (map(TempLEDRedPWM, 0, 255, 0, 4095) / map(LEDBrightness, 0, 4095, 1, 10)));
+    analogWrite(TempLEDGreenPin, (map(TempLEDGreenPWM, 0, 255, 0, 4095) / map(LEDBrightness, 0, 4095, 1, 10)));
+    analogWrite(TempLEDBluePin, (map(TempLEDBluePWM, 0, 255, 0, 4095) / map(LEDBrightness, 0, 4095, 1, 10)));
+  }
 
 
-  
+
 }
